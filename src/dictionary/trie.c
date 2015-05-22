a962aa61f27ff8215554c953857d037bede70e36
js347267 <js347267@students.mimuw.edu.pl>
2015-05-22 22:49:38 +0200

    zaimplementowano add()

--- src/dictionary/trie.c
+++ src/dictionary/trie.c
@@ -1,6 +1,7 @@
 
 #include "trie.h"
 #include <string.h>
+#include <stdlib.h>
 
 /*
 	struct Node
@@ -42,17 +43,17 @@ int startsWith(const char* haystack, const char* needle) //0 - nie, 1 - tak
 	return 1;
 }
 
-
+/// root bedzie wezlem atrapa - nie bedziemy przechowywac w nim stringa
 void Tree_init(struct Tree *t)
 {
-	t->root = NULL;
+	t->root = (struct Node*)(malloc(sizeOf(struct Node)));
+	Node_init(t->root);
 }
 
 void Node_init(struct Node *n)
 {
 	n->string = NULL;
 	n->parent = NULL;
-	n->children = NULL;
 	n->childCount = 0;
 }
 
@@ -71,8 +72,42 @@ void Node_destroy(struct Node *n)
 	free(n->children);
 }
 
-void add(struct Tree *t);
-int find(struct Tree *t); //1 - slowo jest w slowniku; 0 - nie ma
+void add(struct Tree *t, char* word)
+{
+	//if(find(t, word) == 1)) return; //?? zmniejsza efektywnosc
+	addNode(t->root, word);
+
+}
+
+void addNode(struct Node *n, char* word)
+{
+	//todo: co zakladamy wchodzac do wezla?
+	//1. ze nie jest null
+
+	if(*word == '') return;
+
+	for (int i = 0; i < n->childCount; ++i)
+	{
+		if( strcmp(word[0], n->string) == 0 )
+		{
+			addNode(n->children[i], ++word);
+			return;
+		}
+	}
+
+	//jezeli nie znaleziono
+	childCount++;
+	struct Node* newNode = (struct Node*)(malloc(sizeOf(struct Node)));
+	n->children[childCount - 1] = newNode;
+	Node_init(newNode);
+	newNode->parent = n;
+
+}
+
+
+
+
+int find(struct Tree *t, char* word); //1 - slowo jest w slowniku; 0 - nie ma
 
 size_t sizeOf(struct Tree *t);
 
--- src/dictionary/trie.h
+++ src/dictionary/trie.h
@@ -24,8 +24,8 @@
 	void Tree_init(struct Tree *t);
 	void Tree_destroy(struct Tree *t);
 
-	void add(struct Tree *t);
-	int find(struct Tree *t); //1 - slowo jest w slowniku; 0 - nie ma
+	void add(struct Tree *t, char* word);
+	int find(struct Tree *t, char* word); //1 - slowo jest w slowniku; 0 - nie ma
 
 	size_t sizeOf(struct Tree *t);
 
