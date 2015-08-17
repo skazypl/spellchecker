d2c1de6303483a29218d92ad50ece6f04397fed6
js347267 <js347267@students.mimuw.edu.pl>
2015-08-17 14:30:40 +0200

    dodano konstruktor node, oraz kilka funkcji pomocniczych do wczytywania drzewa z pliku

--- src/dictionary/trie.c
+++ src/dictionary/trie.c
@@ -15,6 +15,26 @@ void Node_init(struct Node *n)
 	n->childCount = 0;
 };
 
+struct Node* createNode(int childCount, wchar_t key, struct Node* parent)
+{
+	struct Node* toReturn = (struct Node*)malloc(sizeof(struct Node));
+	Node_init(toReturn);
+
+	toReturn->childCount = childCount;
+	toReturn->parent = parent;
+	toReturn->key = key;
+
+	toReturn->children = (struct Node**)
+		malloc(sizeof(struct Node*) * childCount);
+
+	for (int i = 0; i < childCount; ++i)
+	{
+		toReturn->children[i] = NULL;
+	}
+
+	return toReturn;
+}
+
 void Node_destroy(struct Node *n)
 {
 	for (int i = 0; i < n->childCount; ++i)
@@ -215,7 +235,26 @@ void list_init(struct List* l)
 	l->next = NULL;
 	l->prev = NULL;
 	l->elem = NULL;
-}	
+}
+
+struct List* list_add(struct Node* n, struct List* l)
+{
+	struct List* toReturn = (struct List*)malloc(sizeof(struct List));
+	toReturn->elem = n;
+	toReturn->next = l;
+	return toReturn;
+}
+
+void list_destroy(struct List* l)
+{
+	struct List* toDelete;
+	while(l != NULL)
+	{
+		toDelete = l;
+		l = l->next;
+		free(toDelete);
+	}
+}
 
 //@}
 
@@ -493,6 +532,47 @@ algorytm wczytywania:
 */
 
 
+int FreeTillRoot(struct List* l) 
+//sprawdza czy wierzcholki z listy az do roota wlacznie nie ma wolnych krawedzi
+//lista przechowuje sciezke od pewnego node'a do roota
+{
+	while(l != NULL)
+	{
+		struct Node* n = l->elem;
+
+		for (int i = 0; i < n->childCount; ++i)
+		{
+			if(n->children[i] == NULL)
+				return 0;
+		}
+
+		l = l->next;
+	}
+	return 1;
+}
+
+int hasFree(struct Node* n)
+//zakladamy ze node ma zainicjalizowaną tablicę
+//moze mozna zintegrowac te funkcje z firstFree ???
+{
+	for (int i = 0; i < n->childCount; ++i)
+	{
+		if(n->children[i] == NULL)
+			return 1;
+	}
+	return 0;
+}
+
+int firstFree(struct Node* n)
+{
+	for (int i = 0; i < n->childCount; ++i)
+	{
+		if(n->children[i] == NULL)
+			return i;
+	}
+	return -1;
+}
+
 struct Tree* Tree_load_DFS(FILE* stream)
 {
 	int liczbaNodow = 1; // root
@@ -500,12 +580,58 @@ struct Tree* Tree_load_DFS(FILE* stream)
 	int numb;
 	struct Tree* toReturn = (struct Tree*)(malloc(sizeof(struct Tree)));
 	Tree_init(toReturn);
-	int oldSum; //suma liczby dzieci z tablicy z poprzedniego kroku petli
-	if (fscanf(stream, "%i", &oldSum) == EOF) 
+	int scanChildNum; //suma liczby dzieci z tablicy z poprzedniego kroku petli
+	if (fscanf(stream, "%i", &scanChildNum) == EOF) 
 	//najpierw wczytujemy ilu synow ma root
 		return NULL;
 
-	
+	wchar_t key;
+	int count;
+	int error = 0;
+	struct Node* actual = t->root; //todo: dodac tablice dzieci roota scanChNr
+	struct List* listTillRoot;
+	list_init(listTillRoot);
+
+	while(true)
+	{
+		if (fscanf(stream, "%lc %i", key, count) == EOF)))
+		{
+			/*
+			TODO -> Dodać error handling
+
+			if(FreeTillRoot(listTillRoot) == 0)
+				error = 1; //lub zwyczajnie koniec pliku 
+						//funkcja wyzej zdaje sie wyczerpywac mozliwe errory
+			*/
+			break;
+		}
+
+		if(key == L'!')
+		{
+			actual->children[0] = createNode(count, L'\0', actual); // >\0<? spr
+			while(actual != NULL)
+			{
+				if(hasFree(actual) == 1)
+					break;
+				actual = actual->parent;
+
+			}
+		}
+		else
+		{
+			/* if(firstFree(actual) == -1) error = true; */
+			int freePlace = firstFree(actual);
+			actual->children[freePlace] = createNode(count, key, actual);
+			//firstFree nie bedzie -1 bo dbamy o to w petli
+			actual = actual->children[freePlace];
+		}
+
+	}
+
+	if (error)
+	{
+		//zwolnic pamiec itd, zwrocic null
+	}
 
 	return toReturn;
 
